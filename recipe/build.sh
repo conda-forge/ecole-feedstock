e08aa88cffe2f3ae097fd9f2ad2298129860d041
Antoine Prouvost <AntoinePrv@users.noreply.github.com>
2021-04-08 11:51:57 -0400

    Change Python variable in Cmake

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,6 +10,6 @@ if [[ "$target_platform" == osx-* ]]; then
 	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
 fi
 
-cmake -B build -S "${SRC_DIR}" -D CMAKE_BUILD_TYPE=Release -D PYTHON_EXECUTABLE="${PYTHON}"
+cmake -B build -S "${SRC_DIR}" -D CMAKE_BUILD_TYPE=Release -D Python_EXECUTABLE="${PYTHON}"
 cmake --build build --parallel ${CPU_COUNT}
 "${PYTHON}" -m pip install --no-deps build/python/

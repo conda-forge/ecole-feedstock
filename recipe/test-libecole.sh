82f2e00830b75d9ab2ab4a65815079cfa4918b0b
AntoinePrv <AntoinePrv@users.noreply.github.com>
2021-10-13 13:46:16 -0400

    Add C++17 for libecole test

--- recipe/test-libecole.sh
+++ recipe/test-libecole.sh
@@ -4,6 +4,11 @@ set -o errexit
 set -o pipefail
 set -o nounset
 
+# For using C++17 symbols on Apple before they are officially released
+if [[ "$target_platform" == osx-* ]]; then
+	export CXXFLAGS="${CXXFLAGS} -D_LIBCPP_DISABLE_AVAILABILITY"
+fi
+
 cmake -B build -G Ninja -S examples/libecole
 cmake --build build --parallel ${CPU_COUNT}
 ./build/branching

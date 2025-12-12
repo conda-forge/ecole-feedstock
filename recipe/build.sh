92bc868f52fdfcfc0c9141e4651ce0c87de6cff4
AntoinePrv <AntoinePrv@users.noreply.github.com>
2025-12-12 11:48:35 +0100

    Minimal CMake args

--- recipe/build.sh
+++ recipe/build.sh
@@ -40,13 +40,11 @@ if [[ "${PKG_NAME}" == "ecole" ]]; then
 
 	if [[ "${build_platform}" != "${target_platform}" ]]; then
 		Python_INCLUDE_DIR="$(python -c 'import sysconfig; print(sysconfig.get_path("include"))')"
-		Python_NumPy_INCLUDE_DIR="$(python -c 'import numpy; print(numpy.get_include())')"
 		CMAKE_ARGS="${CMAKE_ARGS} -DPython_EXECUTABLE:PATH=${PYTHON}"
 		CMAKE_ARGS="${CMAKE_ARGS} -DPython_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
-		CMAKE_ARGS="${CMAKE_ARGS} -DPython_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
 		CMAKE_ARGS="${CMAKE_ARGS} -DPython3_EXECUTABLE:PATH=${PYTHON}"
 		CMAKE_ARGS="${CMAKE_ARGS} -DPython3_INCLUDE_DIR:PATH=${Python_INCLUDE_DIR}"
-		CMAKE_ARGS="${CMAKE_ARGS} -DPython3_NumPy_INCLUDE_DIR=${Python_NumPy_INCLUDE_DIR}"
+		export CMAKE_ARGS
 	fi
 
 
